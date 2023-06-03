f810c39b17031d960d988d28ad495461b509fcc5
Kanza Az Zahrawani <09021382227157@studen.unsri.ac.id>
2023-06-03 20:15:59 +0700

    buat file maven java

---
+++ belajar-spring-web-mvc/src/main/java/programmerzamannow/webMVC/BelajarSpringWrebMvcApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.webMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringWrebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringWrebMvcApplication.class, args);
+	}
+
+}
---
+++ belajar-spring-web-mvc/src/test/java/programmerzamannow/webMVC/BelajarSpringWrebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.webMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringWrebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
